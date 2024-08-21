8108ee0ac50d379d3f77c641d20569cbbc2a9150
Ivan Ostashchenko <147530896+IvanOstaschenko@users.noreply.github.com>
2024-08-21 15:14:10 +0300

    change image path prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -39,14 +39,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[path][name].[ext]',
-            },
-          },
-        ],
+        type: 'asset/resource',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
